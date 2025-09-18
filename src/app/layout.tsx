1a4e7f95782c4cda1bbc12ab3006a01723325ef5
0leodev <leojimdev@gmail.com>
2025-09-18 17:14:03 -0400

    boldering...

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+import "./globals.css";
+import WalletProviders from "@/providers/WalletProviders";
+import TopBar from "@/components/TopBar";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <WalletProviders>
+          <TopBar />
+          {children}
+        </WalletProviders>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import EthBalance from "@/components/EthBalance";
+
+export default function Home() {
+  return (
+      <main className="max-w-sm mx-auto p-4">
+        <EthBalance />
+      </main>
+  );
+}
