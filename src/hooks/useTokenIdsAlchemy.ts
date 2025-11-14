8fbe1f11a28ae062bfee968155e55a640622b03a
0leodev <leojimdev@gmail.com>
2025-11-14 19:13:22 -0400

    main structure of the call to alchemy made

--- src/hooks/useTokenIdsAlchemy.ts
+++ src/hooks/useTokenIdsAlchemy.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAccount } from "wagmi";
 import contractAddresses from "@/addresses/11155111.json";
 
@@ -8,10 +8,27 @@ interface UseTokenIdsAlchemyReturn {
   error: Error | null;
 }
 
-export function useTokenIdsAlchemy() {
+export function useTokenIdsAlchemy(): UseTokenIdsAlchemyReturn {
   const { address, chainId } = useAccount();
   const [tokenIds, setTokenIds] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
   const contractAddress = contractAddresses.branches[0].troveNFT
+
+  useEffect(() => {
+    const fetchTokenIds = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+      } catch(err) {
+
+      } finally {
+      setIsLoading(false);
+      }
+    };
+
+    fetchTokenIds();
+  }, []);
+
+  return { tokenIds, isLoading, error };
 }
