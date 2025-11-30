243a11fc68b1f1a9c4508b186f04318c87db9a9d
0leodev <leojimdev@gmail.com>
2025-11-30 16:31:11 -0400

    add metadata on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
-"use client";
 import "./globals.css";
 import WalletProviders from "@/providers/WalletProviders";
 import { Navigation } from "@/components/navigation";
 import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export const metadata: Metadata = {
+  title: "Bolder",
+  description: "Borrow BOLD using ETH, wstETH and rETH ",
+  keywords: "borrow, bold, dapp, bolder, eth, wsteth, reth",
+  authors: [{ name: "leo.dev" }],
+  icons: {
+    icon: "/logos/BOLDER.svg",
+  },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -17,9 +23,7 @@ export default function RootLayout({
       <body>
         <WalletProviders>
           <Navigation />
-          <div className="pt-10">
-            {children}
-          </div>
+          <div className="pt-10">{children}</div>
         </WalletProviders>
         <Toaster />
       </body>
