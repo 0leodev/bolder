aaf5b092f467f763acef42d225b49e9d8b706aee
0leodev <leojimdev@gmail.com>
2025-12-01 17:46:51 -0400

    add prices on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,13 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { motion } from "framer-motion"
-import DashboardCard from "@/components/dashboard/dashboard-card"
+import { motion } from "framer-motion";
+import DashboardCard from "@/components/dashboard/dashboard-card";
 
 export default function DashboardPage() {
   return (
     <main className="min-h-screen bg-background py-15">
       <div className="container mx-auto max-w-7xl px-4">
         <div className="mb-8">
-
           <motion.h1
             initial={{ opacity: 0, y: -100 }}
             animate={{ opacity: 0.1, y: 0 }}
@@ -31,19 +29,11 @@ export default function DashboardPage() {
               ease: [0.16, 1, 0.3, 1],
             }}
           >
-            <div className="flex justify-start gap-4 mb-7">
-              <Link href="/borrow">
-                <button className="rounded-2xl bg-gradient-to-br from-muted/90 to-card/10 px-8 py-4 text-lg font-medium shadow-lg transition-all duration-300 hover:scale-[0.95] hover:from-navigation/90 active:scale-[0.98]">
-                  <span>New Borrow</span>
-                </button>
-              </Link>
-            </div>
-            
             <DashboardCard />
           </motion.div>
-
         </div>
       </div>
     </main>
-  )
+  );
 }
+
--- src/components/dashboard/dashboard-card.tsx
+++ src/components/dashboard/dashboard-card.tsx
@@ -1,9 +1,29 @@
 import TrovesCard from "@/components/dashboard/trove-card"
+import Link from "next/link";
+import { COLLATERAL_TYPES } from "@/lib/constants";
+import { usePriceFeeds } from "@/hooks/usePriceFeeds";
 
 export default function DashboardCard() {
-
+  const prices = usePriceFeeds();
   return(
     <main>
+      <div className="flex justify-between gap-4 mb-7 items-center">
+        <Link href="/borrow">
+          <button className="rounded-2xl bg-gradient-to-br from-muted/90 to-card/10 px-8 py-4 text-lg font-medium shadow-lg transition-all duration-300 hover:scale-[0.95] hover:from-navigation/90 active:scale-[0.98]">
+            <span>New Borrow</span>
+          </button>
+        </Link>
+
+        { prices["WETH"] > 0 && (
+        <div className="flex justify-start gap-2 bg-navigation p-3 rounded-[25px] font-medium text-muted-foreground w-fit">
+          {COLLATERAL_TYPES.map((coll) => (
+            <div className={`flex justify-center gap-2 bg-card px-5 py-3 rounded-2xl min-w-34 ${coll.symbol !== 'WETH' ? 'hidden md:flex' : ''}`}>
+              <img src={coll.icon} className="w-6 h-6" />
+              <div>{`$${prices[coll.symbol].toLocaleString()}`}</div>
+            </div>
+          ))}
+        </div>)}
+      </div>
       <TrovesCard/>
     </main>
   )
