732f81f29bd68e0304046639152e71ab9c741ac5
0leodev <leojimdev@gmail.com>
2025-12-02 16:48:23 -0400

    add hidden to logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,7 +14,7 @@ export default function Footer() {
     <footer className="bg-navigation absolute bottom-0 left-0 right-0">
       <main className="p-10">
         <div className="flex justify-between items-center">
-          <div className="w-17 social-links flex justify-start items-center gap-4">
+          <div className="w-17 flex justify-start items-center gap-4">
             {socialLinks.map((link, index) => (
               <a key={index} href={link.href} target="_blank" rel="noopener noreferrer" className="hover:opacity-70">
                 <Image src={link.icon} alt={link.label} width={26} height={26} />
@@ -23,16 +23,16 @@ export default function Footer() {
           </div>
 
           { prices["WETH"] > 0 && (
-          <div className="flex justify-start gap-2 bg-navigation p-3 rounded-[25px] font-medium text-lg text-muted-foreground w-fit">
+          <div className="flex justify-start gap-2 rounded-[25px] font-medium text-lg text-muted-foreground w-fit">
             {COLLATERAL_TYPES.map((coll) => (
-              <div key={coll.symbol} className={`flex justify-center gap-2 bg-card px-5 py-3 rounded-2xl min-w-34 ${coll.symbol !== 'WETH' ? 'hidden md:flex' : ''}`}>
+              <div key={coll.symbol} className={`flex justify-center gap-2 bg-card px-5 py-3 rounded-2xl min-w-34 ${coll.symbol !== 'WETH' ? 'hidden lg:flex' : ''}`}>
                 <Image src={coll.icon} alt={coll.symbol} width={24} height={24} />
                 <div>{`$${prices[coll.symbol].toLocaleString()}`}</div>
               </div>
             ))}
           </div>)}
 
-          <div className="w-17 flex justify-end">
+          <div className="w-17 flex justify-end hidden sm:flex">
             <Image src="/logos/BOLDER.svg" alt="BOLDER" width={50} height={50} />
           </div>
         </div>
