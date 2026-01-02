7c4e7c3aae07976d399b9703d7dda5a82430e9e0
0leodev <leojimdev@gmail.com>
2026-01-01 21:00:58 -0400

    Add ETH on PriceFeeds & WETH in coingecko fetching

--- src/hooks/usePriceFeeds.ts
+++ src/hooks/usePriceFeeds.ts
@@ -1,11 +1,12 @@
-import { useState, useEffect } from 'react';
-import { coingecko } from '@/config/env';
+import { useState, useEffect } from "react";
+import { coingecko } from "@/config/env";
 import { sBOLD } from "@/abi/sBOLD";
 import { useReadContract } from "wagmi";
 import contractAddresses_1 from "@/addresses/1.json";
 import { formatUnits } from "viem";
 
 export interface PriceFeeds {
+  ETH: number;
   WETH: number;
   wstETH: number;
   rETH: number;
@@ -17,17 +18,18 @@ const twoDigits = (num: number) => parseFloat(num.toFixed(2));
 
 export function usePriceFeeds(): PriceFeeds {
   const [prices, setPrices] = useState<PriceFeeds>({
-    WETH: 0.00,
-    wstETH: 0.00,
-    rETH: 0.00,
-    BOLD: 0.00,
-    sBOLD: 0.00,
+    ETH: 0.0,
+    WETH: 0.0,
+    wstETH: 0.0,
+    rETH: 0.0,
+    BOLD: 0.0,
+    sBOLD: 0.0,
   });
 
   const { data: sBoldRate } = useReadContract({
     address: contractAddresses_1.sBoldToken as `0x${string}`,
     abi: sBOLD,
-    functionName: 'getSBoldRate',
+    functionName: "getSBoldRate",
     chainId: 1,
   });
   const sBoldRateParsed = sBoldRate ? parseFloat(formatUnits(sBoldRate, 18)) : 0;
@@ -35,19 +37,20 @@ export function usePriceFeeds(): PriceFeeds {
   useEffect(() => {
     const fetchPrices = async () => {
       try {
-        const url = `https://api.coingecko.com/api/v3/simple/price?ids=ethereum,wrapped-steth,rocket-pool-eth&vs_currencies=usd&x_cg_demo_api_key=${coingecko.API_KEY}`;
+        const url = `https://api.coingecko.com/api/v3/simple/price?ids=ethereum,weth,wrapped-steth,rocket-pool-eth&vs_currencies=usd&x_cg_demo_api_key=${coingecko.API_KEY}`;
         const response = await fetch(url);
         const data = await response.json();
 
         setPrices({
-          WETH: twoDigits(data.ethereum?.usd),
-          wstETH: twoDigits(data['wrapped-steth']?.usd),
-          rETH: twoDigits(data['rocket-pool-eth']?.usd),
+          ETH: twoDigits(data.ethereum?.usd),
+          WETH: twoDigits(data.weth?.usd),
+          wstETH: twoDigits(data["wrapped-steth"]?.usd),
+          rETH: twoDigits(data["rocket-pool-eth"]?.usd),
           BOLD: 1,
           sBOLD: sBoldRateParsed,
         });
       } catch (error) {
-        console.error('Failed to fetch price feeds:', error);
+        console.error("Failed to fetch price feeds:", error);
       }
     };
 
